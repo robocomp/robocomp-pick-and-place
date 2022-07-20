7fd27db2ff03cf356fb80283f180ce195656f9b2
robolab-kinova <you@example.com>
2022-07-20 13:46:53 +0200

    continuous environment and DQN example code for cartpole

--- basics/test_env.py
+++ basics/test_env.py
@@ -4,11 +4,12 @@ from EnvKinova_gym import EnvKinova_gym
 env = EnvKinova_gym()
 # print(env.observation_space)
 
+print("BEGIN RESET")
 obs = env.reset()
 
 for k, v in obs.items():
     print(f"{k}: {v}")
-
+print("END RESET")
 # action = env.action_space.sample()
 # next_state, reward, done, info = env.step(action)
 env.close()
