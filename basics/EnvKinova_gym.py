52e544c441bb8913d7cb48c3cb3c4edb712bb79c
vamsianumula <himavamsianumula@gmail.com>
2022-07-30 16:07:41 +0900

    initial commit

--- basics/EnvKinova_gym.py
+++ basics/EnvKinova_gym.py
@@ -1,12 +1,11 @@
 import gym, sys, time, math
 from gym import spaces
-import utilities as U
 import numpy as np
 from numpy import linalg as LA
 sys.path.append('/home/robocomp/software/CoppeliaSim_Edu_V4_3_0_rev10_Ubuntu20_04/programming/zmqRemoteApi/clients/python')
 from zmqRemoteApi import RemoteAPIClient
 
-class EnvKinova_gym(gym.Env):
+class EnvKinova_gym(gym.GoalEnv):
 
     #################################
     ## -- GYM INTERFACE METHODS -- ##
@@ -33,18 +32,16 @@ class EnvKinova_gym(gym.Env):
 
         # SPACES
         self.action_space = spaces.Discrete(9)
-        self.observation_space = spaces.Box(low=-50*np.ones((2,)),high=50*np.ones((2,)),dtype=np.float64)
-        # self.n = (self.observation_space[0].high)^(self.observation_space.n)
+        self.observation_space = spaces.Dict({
+            "observation":spaces.Box(low=-50*np.ones((2,)),high=50*np.ones((2,)),dtype=np.float64),
+            "achieved_goal": spaces.Box(low=-50*np.ones((2,)),high=50*np.ones((2,)),dtype=np.float64),
+            "desired_goal":spaces.Box(low=-50*np.ones((2,)),high=50*np.ones((2,)),dtype=np.float64)
+        })
         self.n=100^2
-        
-        self.goal = [0, 0]
 
     def step(self, action):
-        # print(f"Pre: {action}")
         ac1, ac2 = self.__get_action(action)
-        # print(f"Action:{ac1},{ac2}")
-
-        sim_act = [ac1, ac2, 0, 0, 0]
+        sim_act = [int(ac1), int(ac2), 0, 0, 0]
         
         if self.__interpretate_action(sim_act):
             self.sim.callScriptFunction("do_step@gen3", 1, sim_act)
@@ -54,26 +51,36 @@ class EnvKinova_gym(gym.Env):
 
         observation = self.__observate()
 
-        
-        exit, reward, arrival, far, dist = self.__reward_and_or_exit(observation)
+        exit, reward, _  = self.__reward_and_or_exit()
         self.current_step += 1
         
-        info = {
-            "arrival": arrival,
-            "far": far,
-            "dist": dist,
-        }
+        info = {}
 
         return observation, reward, exit, info
+
+    def compute_reward(self,achieved_goal, desired_goal, info):
+        
+        dist = math.sqrt((achieved_goal[0]-desired_goal[0])**2 + (achieved_goal[1]-desired_goal[1])**2)
+
+        if dist < 0.005:
+            reward=0
+        else:
+            reward=-1
+
+        return reward
 
     def reset(self):
         #print("RESET", "STEP:", self.current_step)
         self.goal = self.sim.callScriptFunction("reset@gen3", 1) 
+        self.observation_space["desired_goal"] = self.goal
 
         self.current_step = 0
         obs = self.__observate()
-        # print(obs)
-        return obs
+
+        self.observation_space["desired_goal"] = self.goal
+        self.observation_space["achieved_goal"] = obs
+    
+        return 
 
     def close(self):
         self.sim.stopSimulation()
@@ -93,29 +100,25 @@ class EnvKinova_gym(gym.Env):
         # return {"distX":obs["dist_x"], "distY":obs["dist_y"]}
         return np.array([obs["dist_x"], obs["dist_y"]])
 
-    def __reward_and_or_exit(self, observation):
+    def __reward_and_or_exit(self):
         exit, reward, arrival, far = False, 0, 0, 0
-        dist = math.sqrt(observation[0]**2 + observation[1]**2)
 
-        if dist > 0.1:
+        achieved_goal = self.observation_space["achieved_goal"]
+        desired_goal = self.observation_space["desired_goal"]
+
+        dist = math.sqrt((achieved_goal[0]-desired_goal[0])**2 + (achieved_goal[1]-desired_goal[1])**2)
+        reward =self.compute_reward(achieved_goal, desired_goal, None)
+        if dist > 0.1 or dist <0.005:
             exit = True
-            reward = -10000
-            far = 1
-        
-        else:
-            reward += (1 - self.__normalize(dist, 0, 1)) * 10
-            
-            if dist < 0.005:
-                exit = True
-                reward += 10000
-                arrival = 1
 
-        return exit, reward, arrival, far, dist
+        return exit, reward, None
 
         ''' SIMPIFIED VERSION
         Goes away:           True, -1
         Reaches the target:  True,  1
         Else:                False, 0 '''
+    
+    
 
     def __normalize(self, x, min_val, max_val):
         return (x - min_val) / (max_val + min_val)
--- basics/train.py
+++ basics/train.py
@@ -1,17 +1,30 @@
 from EnvKinova_gym import EnvKinova_gym
-from stable_baselines3 import DQN
+from stable_baselines3 import DQN, HerReplayBuffer
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.callbacks import CheckpointCallback
 from stable_baselines3.common.env_checker import check_env
+from stable_baselines3.her.goal_selection_strategy import GoalSelectionStrategy
 
 env = EnvKinova_gym()
+goal_selection_strategy = 'future'
 
-check_env(env, warn=True, skip_render_check=True)
+model = DQN(
+    "MultiInputPolicy",
+    env,
+    replay_buffer_class=HerReplayBuffer,
+    # Parameters for HER
+    replay_buffer_kwargs=dict(
+        n_sampled_goal=4,
+        goal_selection_strategy=goal_selection_strategy,
+        online_sampling=True,
+        max_episode_length=200,
+    ),
+    verbose=1,
+)
 
 print(env.observation_space)
 print(env.action_space)
 
-model = DQN('MlpPolicy', env, seed=0, verbose=1,learning_starts=1000, tensorboard_log="/home/vamsianumula/Desktop/logs/")
-checkpoint_callback = CheckpointCallback(save_freq=2000, save_path='/home/vamsianumula/Desktop/logs/checkpoints/', name_prefix='check')
+checkpoint_callback = CheckpointCallback(save_freq=2000, save_path='/home/vamsianumula/Desktop/logs/HER/checkpoints/', name_prefix='HER')
 model.learn(total_timesteps=int(1e5),eval_freq=1000,callback=checkpoint_callback)
 model.save("final")
